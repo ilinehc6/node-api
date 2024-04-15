4aa0396379d31253c5f5608c322a18b5f86bc53a
chenyj <chenyj@chenyj.lan>
2024-04-15 10:03:01 -0500

    fix cast error by findById passing object

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,7 +15,7 @@ const getProducts = asyncHandler(async(req, res) => {
 const getProduct = asyncHandler(async(req, res) => {
     try {
         const {id} = req.params
-        const product = await Product.findById({id});
+        const product = await Product.findById(id);
         res.status(200).json(product)
     } catch (error) {
         res.status(500);
